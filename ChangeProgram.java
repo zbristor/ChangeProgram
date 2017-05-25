cf53134a3b23937cff64b866d97aa45093cde704
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 11:48:06 -0400

    first commit

---
+++ ChangeProgram.java
@@ -0,0 +1,13 @@
+
+	public class ChangeProgram {
+		 public static void main(String[] args) {
+		 double x, y, z;
+		 
+		 x = 5;
+		 y = 9.5;
+		 
+		 z = x * y;
+
+		 System.out.println("The product is " + z);
+		 }
+		}
